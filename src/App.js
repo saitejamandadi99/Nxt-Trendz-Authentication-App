110ff6c9f4284e031c15d0240526840e3cdf2fd7
saitejamandadi99 <saitejamandadi99>
2024-09-26 07:41:49 +0000

    added home component and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {Switch, Route} from 'react-router-dom'
 
 import Header from './components/Header'
+import Home from './components/Home'
 import NotFound from './components/NotFound'
 
 const App = () => (
-  <Switch>
+  <>
     <Header />
-    <Route component={NotFound} />
-  </Switch>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
 )
 
 export default App
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,32 @@
 // Write your JS code here
+import './index.css'
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <div className="detailsContainer">
+        <h1 className="heading">Clothes That Get YOU Noticed</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+          alt="clothes that get you noticed"
+          className="image1"
+        />
+        <p className="para">
+          Fashion is part of the daily air and it does not quite help that it
+          changes all the time.Clothes have always been a marker of the era and
+          we are in a revolution. Your fashion makes you been seen and heard
+          that way you are. So, celebrate the new seasons new and exciting in
+          your own way.
+        </p>
+        <button className="Shopbutton">Shop Now</button>
+      </div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+        alt="clothes that get you noticed"
+        className="image2"
+      />
+    </div>
+  )
+}
+
+export default Home
