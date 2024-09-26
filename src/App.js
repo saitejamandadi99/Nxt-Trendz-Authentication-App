30f38638951e27478bc547738441eb4ed6a6e8da
saitejamandadi99 <saitejamandadi99>
2024-09-26 07:20:12 +0000

    added Header Component and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Switch, Route} from 'react-router-dom'
 
+import Header from './components/Header'
 import NotFound from './components/NotFound'
 
 const App = () => (
   <Switch>
+    <Header />
     <Route component={NotFound} />
   </Switch>
 )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,89 @@
 // Write your JS code here
+import {Link} from 'react-router-dom'
+
 import './index.css'
 
-const Header = () => {}
+const Header = () => {
+  return (
+    <div>
+      <div className="appContainer">
+        <div className="logoContainer">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+            className="websitelogo"
+          />
+        </div>
+        <ul className="linksContainer">
+          <li>
+            <Link className="linkItem" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="linkItem" to="/product">
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link className="linkItem" to="/cart">
+              Cart
+            </Link>
+          </li>
+          <button type="button" className="button">
+            Logout
+          </button>
+        </ul>
+      </div>
+
+      <div className="appContainerESM">
+        <div className="logoContainer">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+            className="websitelogo"
+          />
+
+          <button type="button" className="button">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+              alt="nav logout"
+              className="navImg"
+            />
+          </button>
+        </div>
+        <ul className="linksContainer">
+          <li>
+            <Link to="/">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+                alt=" nav home"
+                className="navImg"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/product">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+                alt=" nav products"
+                className="navImg"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+                alt=" nav cart"
+                className="navImg"
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 export default Header
